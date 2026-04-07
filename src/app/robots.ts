efeceb1a9ae7fbbc095c2b9241bc6e0e17dc8c77
johnsulf <erlendjohns@gmail.com>
2026-04-07 12:04:39 +0200

    refactor: standardize string quotes in robots and sitemap files

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,10 @@
-import type { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
 
-export const dynamic = 'force-static';
+export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://teambergen.no/sitemap.xml',
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: "https://teambergen.no/sitemap.xml",
   };
 }
